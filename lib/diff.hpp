affbfbbd99fa89f025b977fd8f192b3649814de4
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-10-18 17:29:49 +0200

    Calculate diff by file.

--- lib/diff.hpp
+++ lib/diff.hpp
@@ -12,25 +12,43 @@
 #include <memory>
 #include <iostream>
 #include <fstream>
+#include <map>
+
+namespace fs = std::filesystem;
 
 using namespace std;
+
+string execute_diff(string file) {
+  array<char, 128> buffer;
+  string result;
+  unique_ptr<FILE, decltype(&pclose)> pipe(popen(string("diff -uN .lit/previous/" + file + " " + file).c_str(), "r"), pclose);
+  if (!pipe) {
+    throw runtime_error("popen() failed!");
+  }
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
+  return result;
+}
 
 class Diff {
   public:
     string content;
 
     Diff() {
-      array<char, 128> buffer;
-      string result;
-      unique_ptr<FILE, decltype(&pclose)> pipe(popen("diff --exclude=.lit -ruN lit .", "r"), pclose);
-      if (!pipe) {
-          throw runtime_error("popen() failed!");
-      }
-      while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-          result += buffer.data();
-      }
+      auto current_path = fs::current_path();
 
-      content = result;
+      for (auto& p : fs::directory_iterator(current_path)) {
+        if (p.is_directory() && p.path().filename().string() != ".lit") {
+          for (auto& sp : fs::recursive_directory_iterator(p)) {
+            if (!sp.is_directory()) {
+              content += execute_diff(fs::relative(sp.path(), current_path).string());
+            }
+          }
+        } else if (!p.is_directory()) {
+          content += execute_diff(fs::relative(p.path(), current_path).string());
+        }
+      }
     }
 
     void save(string filepath) const {
