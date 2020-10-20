ff2dc1e049161d418d369cfceada9b4ec780c90a
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-10-21 01:22:40 +0200

    Add `lit status`.

---
+++ lib/status.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+#include "command.hpp"
+#include "diff.hpp"
+
+using namespace std;
+
+class Status: public Command {
+  public:
+    string name() const {
+      return "status";
+    }
+
+    string description() const {
+      return "Lists all files that have been added, removed, or modified, with respect to the currently checked out commit.";
+    }
+
+    int execute(vector<string> arguments) const {
+      if (arguments.size() > 0) {
+        cerr << "No arguments expected!" << endl;
+        return 1;
+      }
+
+      Diff diff;
+      diff.print_status();
+      return 0;
+    }
+};
