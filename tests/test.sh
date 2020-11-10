767a606a040f6e42bff119c3487ac2e0c42e73b6
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-11-10 12:14:18 +0100

    Use LF for test script.

--- tests/test.sh
+++ tests/test.sh
@@ -12,8 +12,8 @@ lit init
 
 echo "== Creating the first commit"
 cat >file1 <<-EOF
-  This is the first line of the first file. 🚀
-  This is the second line of the first file.
+	This is the first line of the first file. 🚀
+	This is the second line of the first file.
 EOF
 
 # Let's check the status. Should look something like this:
@@ -53,8 +53,8 @@ lit checkout r0
 
 # Checking the file content.
 diff -s file1 - <<-EOF
-  This is the first line of the first file. 🚀
-  This is the second line of the first file.
+	This is the first line of the first file. 🚀
+	This is the second line of the first file.
 EOF
 
 echo "== Switching back to r2"
@@ -62,10 +62,10 @@ lit checkout r2
 
 # Checking the file content again.
 diff -s file1 - <<-EOF
-  This is the first line of the first file. 🚀
-  This is the second line of the first file.
-  A third line is added to the first file.
-  A forth line is added.
+	This is the first line of the first file. 🚀
+	This is the second line of the first file.
+	A third line is added to the first file.
+	A forth line is added.
 EOF
 
 echo "== Adding and discarding changes"
@@ -74,26 +74,26 @@ lit checkout
 
 # Let's confirm.
 diff -s file1 - <<-EOF
-  This is the first line of the first file. 🚀
-  This is the second line of the first file.
-  A third line is added to the first file.
-  A forth line is added.
+	This is the first line of the first file. 🚀
+	This is the second line of the first file.
+	A third line is added to the first file.
+	A forth line is added.
 EOF
 
 echo "== Creating another branch"
 lit checkout r0
 mkdir subfolder
 cat >subfolder/file2 <<-EOF
-  This is the first line of the second file.
-  And another line in the second file.
+	This is the first line of the second file.
+	And another line in the second file.
 EOF
 
 # This should be r3.
 lit commit "Add file2"
 
-# o   ← r3 Add file2
-# │ o   r2 Extend file1 even further
-# │ o   r1 Extend file1
+#   o ← r3 Add file2
+# o │   r2 Extend file1 even further
+# o │   r1 Extend file1
 # o─┘   r0 Add file1
 #lit log
 
@@ -110,15 +110,15 @@ test ! -f subfolder/file2
 #
 ## file2 should now be present.
 #diff -s subfolder/file2 - <<-EOF
-#  This is the first line of the second file.
-#  And another line in the second file.
+#	This is the first line of the second file.
+#	And another line in the second file.
 #EOF
 #
-## o─┐ ← r4 Merge r3 into r2
-## │ o   r3 Add file2
-## o │   r2 Extend file1 even further
-## o │   r1 Extend file1
-## o─┘   r0 Add file1
+## o←┐  ← r4 Merge r3 into r2
+## │ └o   r3 Add file2
+## o  │   r2 Extend file1 even further
+## o  │   r1 Extend file1
+## o──┘   r0 Add file1
 #lit log
 #
 #echo "== Setting up a conflict"
@@ -129,13 +129,13 @@ test ! -f subfolder/file2
 #echo >>file1 "Third line on top of r3."
 #lit commit "Extend file1 another way" # r6
 #
-##   o ← r6 Extend file1 another way
-## o │   r5 Extend file1 one way
-## o─┤   r4 Merge r3 into r2
-## │ o   r3 Add file2
-## o │   r2 Extend file1 even further
-## o │   r1 Extend file1
-## o─┘   r0 Add file1
+##    o ← r6 Extend file1 another way
+## o  │   r5 Extend file1 one way
+## o←┐│   r4 Merge r3 into r2
+## │ └o   r3 Add file2
+## o  │    r2 Extend file1 even further
+## o  │    r1 Extend file1
+## o──┘    r0 Add file1
 #lit log
 #
 ## Going back and merging.
@@ -145,24 +145,24 @@ test ! -f subfolder/file2
 ## Let's check all file versions:
 ##   Current commit
 #diff -s file1 - <<-EOF
-#  This is the first line of the first file. 🚀
-#  This is the second line of the first file.
-#  A third line is added to the first file.
-#  A forth line is added.
-#  Fifth line on top of r4.
+#	This is the first line of the first file. 🚀
+#	This is the second line of the first file.
+#	A third line is added to the first file.
+#	A forth line is added.
+#	Fifth line on top of r4.
 #EOF
 #
 ##   other branch
 #diff -s file1.r6 - <<-EOF
-#  This is the first line of the first file. 🚀
-#  This is the second line of the first file.
-#  Third line on top of r3.
+#	This is the first line of the first file. 🚀
+#	This is the second line of the first file.
+#	Third line on top of r3.
 #EOF
 #
 ##  common base
 #diff -s file1.r3 - <<-EOF
-#  This is the first line of the first file. 🚀
-#  This is the second line of the first file.
+#	This is the first line of the first file. 🚀
+#	This is the second line of the first file.
 #EOF
 #
 ## Let's simulate some conflict resolution.
@@ -176,22 +176,22 @@ test ! -f subfolder/file2
 ## Let's verify the final result.
 #
 #diff -s file1 - <<-EOF
-#  This is the first line of the first file. 🚀
-#  This is the second line of the first file.
-#  A third line is added to the first file.
-#  A forth line is added.
-#  Fifth line on top of r4.
-#  Sixth line added during merge conflict.
+#	This is the first line of the first file. 🚀
+#	This is the second line of the first file.
+#	A third line is added to the first file.
+#	A forth line is added.
+#	Fifth line on top of r4.
+#	Sixth line added during merge conflict.
 #EOF
 #
-## o─┐ ← r7 Merge r6 into r5
-## │ o   r6 Extend file1 another way
-## o │   r5 Extend file1 one way
-## o─┤   r4 Merge r3 into r2
-## │ o   r3 Add file2
-## o │   r2 Extend file1 even further
-## o │   r1 Extend file1
-## o─┘   r0 Add file1
+## o←┐   ← r7 Merge r6 into r5
+## │ └─o   r6 Extend file1 another way
+## o   │   r5 Extend file1 one way
+## o←┐ │   r4 Merge r3 into r2
+## │ └─o   r3 Add file2
+## o   │   r2 Extend file1 even further
+## o   │   r1 Extend file1
+## o───┘   r0 Add file1
 #lit log
 
 echo "== Cleanup"
